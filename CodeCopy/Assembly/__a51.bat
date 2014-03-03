7eca3cf78d6eff6145d0659d470ceea2a22bdfdc
Nolan Ramsden <nolan_ramsden@hotmail.com>
2014-03-02 22:26:59 -0800

    Delete __a51.bat

--- CodeCopy/Assembly/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\Charles\Documents\GitHub\Projects\Code\Assembly\"
-"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Charles\Documents\GitHub\Projects\Code\Assembly\DownCounterAttempt.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Charles\Documents\GitHub\Projects\Code\Assembly\DownCounterAttempt.HEX
