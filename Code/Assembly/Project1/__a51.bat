1d0f71cb0463e61982d7762864a6e2e51c1e7dd4
champ <champ@muramasa.(none)>
2014-03-14 10:57:56 -0700

    Organization time

--- Code/Assembly/Project1/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-Z:
-cd "\home\champ\Engineering\GitHub\Projects\Code\Assembly\Project1\"
-"Z:\home\champ\Engineering\EECE_281\AlteraBoardStuff\a51.exe" -l "Z:\home\champ\Engineering\GitHub\Projects\Code\Assembly\Project1\State Machine.asm"
-echo Crosside_Action Set_Hex_File Z:\home\champ\Engineering\GitHub\Projects\Code\Assembly\Project1\State Machine.HEX
