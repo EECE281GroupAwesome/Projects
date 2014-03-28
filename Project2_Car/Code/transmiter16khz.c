984595f00175e7e9b443642488a59bcf602704f8
Brad MacNeil <brad.macneil5@gmail.com>
2014-03-28 09:48:45 -0700

    Basic Transmitter code
    frame work need more attention

---
+++ Project2_Car/Code/transmiter16khz.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <at89lp51rd2.h>
+
+// ~C51~ 
+ 
+#define CLK 22118400L
+#define BAUD 115200L
+#define BRG_VAL (0x100-(CLK/(32L*BAUD)))
+
+//We want timer 0 to interrupt every 100 microseconds ((1/10000Hz)=100 us)
+#define FREQ 30300L
+#define TIMER0_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
+
+
+void wait_bit_time(void );
+
+volatile bit txon = 1; 
+
+
+
+unsigned char _c51_external_startup(void)
+{
+	// Configure ports as a bidirectional with internal pull-ups.
+	P0M0=0;	P0M1=0;
+	P1M0=0;	P1M1=0;
+	P2M0=0;	P2M1=0;
+	P3M0=0;	P3M1=0;
+	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
+	P4M0=0;	P4M1=0;
+	
+	P3M0 &= 0B_1111_0011;
+	P3M1 |= 0B_0000_1100;
+    
+    /*
+    // Initialize the serial port and baud rate generator
+    PCON|=0x80;
+	SCON = 0x52;
+    BDRCON=0;
+    BRL=BRG_VAL;
+    BDRCON=BRR|TBCK|RBCK|SPD;
+    */
+    	// Initialize timer 0 for ISR 'pwmcounter()' below
+	TR0=0; // Stop timer 0
+	TMOD=0x01; // 16-bit timer
+	// Use the autoreload feature available in the AT89LP51RB2
+	// WARNING: There was an error in at89lp51rd2.h that prevents the
+	// autoreload feature to work.  Please download a newer at89lp51rd2.h
+	// file and copy it to the crosside\call51\include folder.
+	TH0=RH0=TIMER0_RELOAD_VALUE/0x100;
+	TL0=RL0=TIMER0_RELOAD_VALUE%0x100;
+	TR0=1; // Start timer 0 (bit 4 in TCON)
+	ET0=1; // Enable timer 0 interrupt
+	EA=1;  // Enable global interrupts
+	
+	//pwmcount=0;
+    
+    return 0;
+}
+
+void wait_bit_time (void)
+{
+	_asm	
+		;For a 22.1184MHz crystal one machine cycle 
+		;takes 12/22.1184MHz=0.5425347us
+	    mov R2, #2
+	L3:	mov R1, #248
+	L2:	mov R0, #184
+	L1:	djnz R0, L1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	    djnz R1, L2 ; 200us*250=0.05s
+	    djnz R2, L3 ; 0.05s*20=1s
+	    ret
+    _endasm;
+}
+
+void pwmcounter (void) interrupt 1
+{
+	if ( txon )
+	{
+		P3_2 = !P3_2;
+		P3_3 = !P3_3;
+	}
+}
+	
+void tx_byte ( unsigned char val )
+{
+	unsigned char j;
+	//Send the start bit
+	txon=0;
+	wait_bit_time();
+	for (j=0; j<8; j++)
+	{
+		txon=val&(0x01<<j)?1:0;
+		wait_bit_time();
+	}
+	txon=1;
+	//Send the stop bits
+	wait_bit_time();
+	wait_bit_time();
+}
+
+void main (void)
+{
+	P3_2 = 0;
+	P3_3 = 1;
+	while(1)
+	{
+	 
+		if( !P1_1 ) tx_byte( 0x55 ); 
+		
+	}	
+	
+}
+
